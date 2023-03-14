341b612b0052eb67db7c57a660ea92f2f955c94c
sahilkhunt45 <sahilkhunt45@gmail.com>
2023-03-14 16:41:53 +0530

    Sahil commited

---
+++ lib/helper/local_notification_helper.dart
@@ -0,0 +1,136 @@
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:timezone/data/latest.dart' as tz;
+// import 'package:timezone/timezone.dart' as tz;
+//
+// class LocalNotificationHelper {
+//   LocalNotificationHelper._();
+//
+//   static final LocalNotificationHelper localNotificationHelper =
+//       LocalNotificationHelper._();
+//
+//   static final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//       FlutterLocalNotificationsPlugin();
+//
+//   Future<void> scheduledNotification() async {
+//
+//     AndroidNotificationDetails _androidNotificationDetails =
+//         const AndroidNotificationDetails(
+//       'channel ID',
+//       'channel name',
+//       playSound: true,
+//       priority: Priority.high,
+//       importance: Importance.high,
+//     );
+//     DarwinNotificationDetails _iosNotificationDetails =
+//         const DarwinNotificationDetails();
+//     NotificationDetails platformChannelSpecifics = NotificationDetails(
+//         android: _androidNotificationDetails, iOS: _iosNotificationDetails);
+//
+//     tz.initializeTimeZones();
+//     await flutterLocalNotificationsPlugin.zonedSchedule(
+//         0,
+//         "Notification Title",
+//         "This is the Notification Body!",
+//         tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+//         platformChannelSpecifics,
+//         androidAllowWhileIdle: true,
+//         uiLocalNotificationDateInterpretation:
+//             UILocalNotificationDateInterpretation.absoluteTime);
+//   }
+// }
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+class LocalNotificationHelper {
+  LocalNotificationHelper._();
+  static final LocalNotificationHelper localNotificationHelper =
+  LocalNotificationHelper._();
+
+  static final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  Future<void> sendSimpleNotification() async {
+    AndroidNotificationDetails androidNotificationDetails =
+    const AndroidNotificationDetails(
+      'demoId',
+      'demo',
+      playSound: true,
+      priority: Priority.max,
+      importance: Importance.max,
+      icon: "mipmap/ic_launcher",
+    );
+    DarwinNotificationDetails darwinNotificationDetails =
+    const DarwinNotificationDetails();
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails, iOS: darwinNotificationDetails);
+    await flutterLocalNotificationsPlugin.show(
+        1, 'Simple Notification', 'Demo', notificationDetails,
+        payload: "My custom Data");
+  }
+
+  Future<void> scheduledNotification() async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+      'channel id',
+      'channel name',
+      channelDescription: 'channel description',
+      icon: 'mipmap/ic_launcher',
+      priority: Priority.max,
+      importance: Importance.max,
+      largeIcon: DrawableResourceAndroidBitmap('mipmap/ic_launcher'),
+    );
+
+    var iOSPlatformChannelSpecifics = DarwinNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      "Notification Title",
+      "This is the Notification Body!",
+      tz.TZDateTime.now(tz.local).add(const Duration(seconds: 10)),
+      platformChannelSpecifics,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
+
+  Future<void> showBigPictureNotification() async {
+    var bigPictureStyleInformation = const BigPictureStyleInformation(
+      DrawableResourceAndroidBitmap("mipmap/ic_launcher"),
+      largeIcon: DrawableResourceAndroidBitmap("mipmap/ic_launcher"),
+      contentTitle: 'flutter devs',
+      summaryText: 'summaryText',
+    );
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'big text channel id', 'big text channel name',
+        channelDescription: 'big text channel description',
+        styleInformation: bigPictureStyleInformation);
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics, iOS: null);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'big text title', 'silent body', platformChannelSpecifics,
+        payload: "big image notifications");
+  }
+
+  Future<void> showNotificationMediaStyle() async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+      'media channel id',
+      'media channel name',
+      channelDescription: 'media channel description',
+      color: Colors.green,
+      fullScreenIntent: true,
+      category: AndroidNotificationCategory("Fire-Base"),
+      playSound: true,
+      enableLights: true,
+      largeIcon: DrawableResourceAndroidBitmap("mipmap/ic_launcher"),
+      styleInformation: MediaStyleInformation(),
+    );
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics, iOS: null);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'notification title', 'notification body', platformChannelSpecifics);
+  }
+}
